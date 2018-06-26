e071618b94f4cba4b44a31d6f8c10f519e4dc292
Chong Vang <vangcguy@gmail.com>
2018-06-26 08:42:40 -0500

    added methods to query class

---
+++ HumaneSociety/Query.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    public static class Query
+    {
+        public static void GetPendingAdoptions()
+        {
+
+        }
+        
+        public static void RunEmployeeQueries()
+        {
+
+        }
+
+        public static void GetClient()
+        {
+
+        }
+
+        public static void GetUserAdoptionStatus()
+        {
+
+        }
+
+        public static void GetAnimalByID()
+        {
+
+        }
+
+        public static void Adopt()
+        {
+
+        }
+
+        public static void RetrieveClients()
+        {
+
+        }
+
+        public static void GetStates()
+        {
+
+        }
+
+        public static void AddNewClient()
+        {
+
+        }
+
+        public static void updateClient()
+        {
+
+        }
+
+        public static void UpdateUsername()
+        {
+
+        }
+
+        public static void UpdateEmail()
+        {
+
+        }
+
+        public static void UpdateAddress()
+        {
+
+        }
+
+        public static void UpdateFirstName()
+        {
+
+        }
+
+        public static void UpdateLastName()
+        {
+
+        }
+
+        public static void UpdateAdoption()
+        {
+
+        }
+
+        public static void GetShots()
+        {
+
+        }
+
+        public static void UpdateShot()
+        {
+
+        }
+
+        public static void EnterUpdate()
+        {
+
+        }
+
+        public static void RemoveAnimal()
+        {
+
+        }
+
+        public static void GetBreed()
+        {
+
+        }
+
+        public static void GetDiet()
+        {
+
+        }
+
+        public static void GetLocation()
+        {
+
+        }
+
+        public static void AddAnimal()
+        {
+
+        }
+
+        public static void EmployeeLogin()
+        {
+
+        }
+
+        public static void RetrieveEmployeeUser()
+        {
+
+        }
+
+        public static void AddUsernameAndPassword()
+        {
+
+        }
+
+        public static void CheckEmployeeUserNameExist()
+        {
+
+        }
+
+    }
+}
